aecb032f3c90e75706bc9de9bb4284664a9bcc99
sztral <rafaelfernandezgalleguillos@outlook.com>
2025-04-03 10:48:40 -0300

    Added views for templates, fixed some urls in template menuprincipal_wik.html

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home
+from . import views
+from .views import forum
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path('foro/', views.forum, name='forum'),
+    path('registrarse/', views.signup, name='signup'),
+    path('iniciar-sesion/', views.login, name='login'),
+    path('micuenta/', views.account, name='account'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'core/menuprincipal_wiki.html')
+
+def forum(request):
+    return render(request, 'core/forowiki.html')
+
+def signup(request):
+    return render(request, 'core/registro_wiki.html')
+
+def login(request):
+    return render(request, 'core/inicio_sesion_wiki.html')
+
+def account(request):
+    return render(request, 'core/micuentatf.html')
